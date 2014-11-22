29743a53909b246c2b5040db3b01cea6ff272153
Kevin Michael Walters <kmw2168@columbia.edu>
2014-11-22 12:57:38 -0500

    updated UserController

--- src/UserController.py
+++ src/UserController.py
@@ -1,11 +1,34 @@
-import hashlib
-import uuid
+from bson.json_util import dumps
+from django.http import HttpResponse
+from pymongo.mongo_client import MongoClient
+
+from System import System
+from models import User
+
 
 class UserController:
+    
+    #@staticmethod
+    #def createUser(...):
+    #TODO...what kind of FB data do we get?
+    
+    
+    @staticmethod
+    def getAllUsers(request):
+        client = MongoClient(System.URI)
+        db = client.obo
+        users = db.user
+        user_list = list()
+        for user_data in users.find():
+            user_list.append(user_data.getJson())
+        return HttpResponse(user_list)        
 
     @staticmethod
-    def salt_password(password, salt=None):
-        if salt == None:
-            salt = uuid.uuid4().hex
-        hashed_password = hashlib.sha512(password + salt).hexdigest()
-        return (hashed_password, salt)
+    def getUser(user_id):
+        client = MongoClient(System.URI)
+        db = client.obo
+        users = db.user
+        user = users.find_one({"_id": user_id})
+        return dumps(user)
+    
+    
