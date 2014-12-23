203d168b306b30de076d6c4dae85866924ca3c5c
Kevin Michael Walters <kmw2168@columbia.edu>
2014-12-23 17:11:31 -0500

    Added comments for UserController

--- ConnectMeApp/UserController.py
+++ ConnectMeApp/UserController.py
@@ -13,6 +13,7 @@ from CalendarController import CalendarController
 
 class UserController:
     
+    #creates a new user - called if this is the first time authenticating a user
     @staticmethod
     def createUser(name, facebookId, accessToken, pictureURL):
         user = User(name, facebookId, accessToken, pictureURL)
@@ -20,28 +21,30 @@ class UserController:
         
         print "created user:", user.id 
         
-        CalendarController.createCalendar(user.id)
+        CalendarController.createCalendar(user.id) #creates a calendar for this user
         
         return user.id
     
+    #login this user; since Facebook handles all authentication, we just need to return a user_id
+    #we also create the user if it is their first time authenticating
     @staticmethod
     def login(name, facebookId, accessToken, pictureURL):
         client = MongoClient(System.URI)
         db = client.ConnectMe
         users = db.user
         
-        user = users.find_one({"facebookId" : facebookId})
+        user = users.find_one({"facebookId" : facebookId}) #checks if this facebook id is already in our database
         
         if user:
             result = user['_id']
         else:
-            user = UserController.createUser(name, facebookId, accessToken, pictureURL)
+            user = UserController.createUser(name, facebookId, accessToken, pictureURL) #create a new user if not in our database
             result = user
         if not user:
             return "fail"
         return result
         
-    
+    #returns a list of all users
     @staticmethod
     def getAllUsers(request):
         client = MongoClient(System.URI)
@@ -52,6 +55,7 @@ class UserController:
             user_list.append(user_data.getJson())
         return HttpResponse(user_list)     
     
+    #returns the full user for this user id
     @staticmethod
     def getUser(user_id):
         try:
@@ -62,24 +66,26 @@ class UserController:
         db = client.ConnectMe
         users = db.user
         
-        user = users.find_one({"_id": user_id})
+        user = users.find_one({"_id": user_id}) #get the user that matches this id
         
         return user
     
+    #given a list of facebookIds, returns their associated user_ids in our own database
     @staticmethod
     def getUsersFromFriends(friends):
         client = MongoClient(System.URI)
         db = client.ConnectMe
         users = db.user
         
-        friendList = users.find({"facebookId": {"$in" : friends}})
+        friendList = users.find({"facebookId": {"$in" : friends}}) #finds all users with facebookId field in the list of facebookIds provided
         
         friends = list()
         for friend in friendList:
-            friends.append(friend['_id'])
+            friends.append(friend['_id']) #generate a list of user_ids
         
         return friends
     
+    #gets the facebook friends of the provided user
     @staticmethod
     def getFacebookFriends(user_id):
         try:
@@ -92,16 +98,18 @@ class UserController:
         #fbUsers = db.social_auth_usersocialauth
         users = db.user
         
-        user = users.find_one({"_id": user_id})
+        user = users.find_one({"_id": user_id}) #get the user object associated with this id
         
         print user
         if not user:
             return "fail"
         
+        #get the user's access token and facebook ID, which were stored in our database
         accessToken = user['access_token']
         print accessToken
         print user['facebookId']
 
+        #get the user's friends
         url = u'https://graph.facebook.com/{0}/' \
             u'friends?fields=id,name,location,picture' \
             u'&access_token={1}'.format(user['facebookId'], accessToken)
@@ -110,17 +118,18 @@ class UserController:
 
         friend_ids = list()
         for friend in friends:
-            friend_ids.append(friend['id'])
+            friend_ids.append(friend['id']) #we only need the friend IDs
             
         return friend_ids
     
+    #given multiple user ids, return the list of their full user objects
     @staticmethod
     def getMultiUser(user_ids):
-        user_ids = user_ids.split(',')
+        user_ids = user_ids.split(',')#ids come in a a comma-separated list
         user_obj_ids = list()
         try:
             for user_id in user_ids:
-                user_obj_ids.append(ObjectId(user_id))
+                user_obj_ids.append(ObjectId(user_id)) #cast each id as an ObjectId
         except:
             return "fail"
         
@@ -129,7 +138,7 @@ class UserController:
         users = db.user
         print user_obj_ids
         
-        userList = users.find({"_id": {"$in" : user_obj_ids}})
+        userList = users.find({"_id": {"$in" : user_obj_ids}}) #find all users with a user_id in the list provided
         
         if not userList:
             return "fail"
