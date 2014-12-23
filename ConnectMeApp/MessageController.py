a791effb00f7255d01f1d9a1f1ab8b722089193c
Kevin Michael Walters <kmw2168@columbia.edu>
2014-12-23 17:30:02 -0500

    Added comments for MessageController

--- ConnectMeApp/MessageController.py
+++ ConnectMeApp/MessageController.py
@@ -2,8 +2,10 @@ import datetime
 from models import Message
 from bson.objectid import ObjectId
 
+#this controller was never implemented as we hit a roadblock, and due to time constraints were unable to do messaging
 class MessageController:
     
+    #create a message for the event
     @staticmethod
     def createMessage(sender_id, event_id, message, time):
         try:
@@ -16,14 +18,15 @@ class MessageController:
         if not message_id:
             return "fail"
         
+    #send a message for the event
     @staticmethod
     def sendMessage(sender_id, event_id, message):
         time = datetime.datetime.now()
         result = MessageController.createMessage(sender_id, event_id, message, time)
         if result == "fail":
             return "fail"
-        MessageController.sendSNS(sender_id, event_id, message, time)
+        MessageController.sendSNS(sender_id, event_id, message, time)#use SNS to send to all monitoring that event
         
     @staticmethod
     def sendSNS(sender_id, event_id, message, time):
-        #TODO
+        pass#TODO
